36500ab108845e1bc8276e5c9eadb4c2d546ecab
nazhdaque <nazhdaque@gmail.com>
2023-07-23 09:58:01 +0300

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./css/main.css";
+
+console.log(
+	"%cCoded by ✨Nazhdaque✨\nhttps://www.frontendmentor.io/profile/Nazhdaque/solutions",
+	"background: #222; color: chartreuse; font-size: 1.25rem"
+);
